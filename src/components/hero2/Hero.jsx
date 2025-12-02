4fd568730e6b46f6d1b985c69761d867397aa270
SHLcomp <salibahe123@gmail.com>
2025-12-02 18:17:58 +0300

    tried gsap for the title animation and removed the css animation

--- src/components/hero2/Hero.jsx
+++ src/components/hero2/Hero.jsx
@@ -2,11 +2,24 @@ import React, { useEffect, useRef } from "react";
 import "./Hero.scss";
 import globe from "../../assets/earth.png";
 import marble from "../../assets/marble.png";
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
 
+gsap.registerPlugin(SplitText);
 export const Hero = () => {
   const descRef = useRef(null); // ref for description
 
   useEffect(() => {
+    
+  let split = new SplitText(".split", { type: "chars" });
+  let chars = split.chars;
+
+  gsap.from(chars, {
+    yPercent: 130,
+    stagger: 0.009,
+    ease: "power4.out",
+  });
+
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
@@ -21,7 +34,10 @@ export const Hero = () => {
 
     if (descRef.current) observer.observe(descRef.current); // observe description
 
-    return () => observer.disconnect();
+    return () => {
+      observer.disconnect();
+      split.revert();
+    };
   }, []);
 
   return (
@@ -30,7 +46,6 @@ export const Hero = () => {
         <img className="globe" src={globe} alt="" />
       </div>
       <div className="hero-text">
-        
         <div className="desc" ref={descRef}>
           <p>
             Light of the Universe crafts extraordinary experiences through
@@ -47,11 +62,10 @@ export const Hero = () => {
           </div>
         </div>
         <div className="hero-title">
-          <h1 className="golden-glow-text">
+          <h1 className="golden-glow-text split">
             Your ideas. Our light. Universal impact.
           </h1>
         </div>
-        
       </div>
 
       <div
@@ -69,18 +83,17 @@ export const Hero = () => {
         </ul>
       </div>
 
-      
       <div className="info">
-          <p>
-             Completed Project <span>500+</span>
-          </p>
-          <p>
-             Years of Expertise <span>15+</span>
-          </p>
-          <p>
-            Satisfaction  <span>100%</span>
-          </p>
-        </div>
+        <p>
+          Completed Project <span>500+</span>
+        </p>
+        <p>
+          Years of Expertise <span>15+</span>
+        </p>
+        <p>
+          Satisfaction <span>100%</span>
+        </p>
+      </div>
     </div>
   );
 };
